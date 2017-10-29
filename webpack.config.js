d21b448772d510196ce0db0d19b81479a110ad3b
Raheel Hassan <syed.hassan.lhr@gmail.com>
2017-10-29 13:14:38 +0500

    fixed issues with css-loader & package.json

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,10 @@ var config = {
                 query: {
                     presets: ["react", "es2015"]
                 }
+            },
+            {
+              test: /\.css$/,
+              use: [ 'style-loader', 'css-loader' ]
             }
         ]
     }
